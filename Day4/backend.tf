6f3e31d84e0f1e47b638162a140fc0bcecfc15ab
Yogesh Bhujbal <bhujbalyogesh123@gmail.com>
2025-08-04 17:50:14 +0530

    Delete Day4 directory

--- Day4/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "mytestbucketfordevops23012025"
-    region = "ap-south-1"
-    key = "folder1/terraform.tfstate"
-    encrypt        = true
-    dynamodb_table = "terraform-lock"
-  }
-}
